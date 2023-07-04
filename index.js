babdccfec60adee140e9c8b1c4d910131c06c90b
Daniel Odey <dannyandcode@gmail.com>
2023-07-04 06:36:08 -0400

    number challenge

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//capitalize challenge
+
 const myString = 'developer';
 
 let myNewString;
@@ -5,3 +7,33 @@ let myNewString;
 myNewString = myString.slice(0,1).toUpperCase() + myString.slice(1);
 
 console.log(myNewString);
+
+//number challenge
+
+let x = Math.round(Math.random() * 100 + 1);
+
+let y = Math.round(Math.random() * 50 + 1);
+
+let sum = `${x} + ${y} = ${x + y}`; 
+
+let difference = `${x} - ${y} = ${x - y}`;
+
+let product = `${x} * ${y} = ${x * y}`;
+
+let quotient = `${x} / ${y} = ${x / y}`;
+
+let remainder = `${x} % ${y} = ${x % y}`;
+
+console.log(remainder);
+
+console.log(quotient);
+
+console.log(product);
+
+console.log(difference);
+
+console.log(sum);
+
+console.log(x);
+
+console.log(y);
