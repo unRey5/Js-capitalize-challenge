e71801c2a7625a21eb9cb8e327f47fef23408ff3
Daniel Odey <dannyandcode@gmail.com>
2023-07-19 10:21:09 -0400

    object challenge start

--- index.js
+++ index.js
@@ -204,3 +204,44 @@ const todo3 = {
 const { title } = todo3;
 
 console.log(title);
+
+//object challenge
+
+//Create an array of objects called `library`. Add 3 objects with a property of `title`, `author`, `status`. Title and author should be strings (whatever value you want) and status should be another object with the properties of `own`, `reading` and `read`. Which should all be boolean values. For all status, set `own` to `true` and `reading` and `read` to false.
+
+const library = [
+    {
+        title: 'My object assignment',
+        author: 'Daniel Defoe',
+        status: {
+            own: true,
+            reading: false,
+            read: false,
+        },
+    },
+    {
+        title: 'My object assignment',
+        author: 'Daniel Defoe',
+        status: {
+            own: true,
+            reading: false,
+            read: false,
+        },
+    },
+    {
+        title: 'My object assignment',
+        author: 'Daniel Defoe',
+        status: {
+            own: true,
+            reading: false,
+            read: false,
+        },
+    },
+];
+
+console.log(library);
+
+//You finished reading all of the books. Set the `read` value for all of them to `true`. Do not edit the initial object. Set the values using dot notation.
+let b;
+b = library.status;
+console.log(b);
